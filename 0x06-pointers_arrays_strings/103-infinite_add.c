b64cd6aa1a923d37591479ced9bc8eb69a0aabeb
AdebowaleGideon <gideonadebowale66@outlook.com>
2023-01-10 21:54:10 +0000

    betty compliance

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,7 +14,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	rem = l1 = l2 = 0;
 	while (n1[l1] != '\0')
-	       l1++;
+		l1++;
 	while (n2[l2] != '\0')
 		l2++;
 	max = l2 > l1 ? l2 : l1;
